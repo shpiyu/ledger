0ab16651b5cd7141113614cc5b438018b3018665
Piyush Ranjan <piyushranjan95@gmail.com>
2024-02-18 22:50:52 +0530

    add date navigation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ function App() {
   const [amount, setAmount] = useState('');
   const totalAmount = items.reduce((total, item) => total + parseInt(item.amount), 0);
 
+  const [date, setDate] = useState(new Date());
+
+  const formatDate = (date) => {
+    const day = date.getDate();
+    const month = date.toLocaleDateString('default', { month: 'short' });
+    const weekday = date.toLocaleDateString('default', { weekday: 'long' });
+    const suffix = (day % 10 === 1 && day !== 11)
+      ? 'st'
+      : (day % 10 === 2 && day !== 12)
+      ? 'nd'
+      : (day % 10 === 3 && day !== 13)
+      ? 'rd'
+      : 'th';
+    return `${weekday} - ${day}${suffix} ${month}`;
+  }
+  
   const handleAdd = (e) => {
     e.preventDefault();
     setItems([...items, { item, amount }]);
@@ -14,10 +30,30 @@ function App() {
     setAmount(0);
   }
 
+  const handlePrevDay = () => {
+    setDate(prevDate => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(prevDate.getDate() - 1);
+      return newDate;
+    });
+  }
+
+  const handleNextDay = () => {
+    setDate(currDate => {
+      const newDate = new Date(currDate);
+      newDate.setDate(currDate.getDate() + 1);
+      return newDate;
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>18th Feb - Sunday</h1>
+        <h1 className='header'>
+          <span className='nav-btn' onClick={handlePrevDay}>&lt;</span>
+            {formatDate(date)}
+          <span className='nav-btn' onClick={handleNextDay}>&gt;</span>
+        </h1>
       </header>
       <div className='container'>
         <div className='ledger'>
